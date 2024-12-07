3d97ebbbac8ca0b9e85361ece9e1d85b27ac8a55
Yogesh sharma <eryogeshrf@gmail.com>
2024-12-07 09:06:08 +0530

    p2-one rg added

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rg1_yogi" {
   name     = "yogi_12"
   location = "eastus"
 }
+
+
+resource "azurerm_resource_group" "rg1_yogi2" {
+  name     = "yogi_112"
+  location = "centralindia"
+}
